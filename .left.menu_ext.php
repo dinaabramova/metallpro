1f9cf03e11c88bac69c7e21ed5e162ebc845d144
Dina <tinsel.tula@gmail.com>
2015-05-01 15:06:22 +0400

    Added catalog menu component

--- .left.menu_ext.php
+++ .left.menu_ext.php
@@ -3,11 +3,12 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 global $APPLICATION;
 
-$aMenuLinksExt = $APPLICATION->IncludeComponent("bitrix:menu.sections", "", array(
+$aMenuLinksExt = $APPLICATION->IncludeComponent("user:catalog.menu", "", array(
     "IS_SEF" => "N",
     "ID" => $_REQUEST["ID"],
     "IBLOCK_TYPE" => "catalog",
     "IBLOCK_ID" => "4",
+    "VIEW_TYPE" => $_REQUEST['VIEW'] ? $_REQUEST['VIEW'] : 'section',
     "SECTION_URL" => "",
     "DEPTH_LEVEL" => "2",
     "CACHE_TYPE" => "A",
---
+++ include/ru/catalog_settings.php
@@ -0,0 +1,25 @@
+<div id="catalog_settings">
+    <span>Настройка древа каталога</span>
+
+    <div class='pop_up'>
+        <div id="choose_sect">
+            <p>Отображение сечений по:</p>
+            <a href="#" data-type="country">Страна</a>
+            <a href="#" data-type="section" class="active">Тип сечения</a>
+        </div>
+        <div id="units">
+            <p>Единицы измерения</p></br>
+            <div class="wrap_units" id="unit_one">
+                <h5>Длина</h5>
+                <a href="#" id="mm">Мм</a>
+                <a href="#" id="cm">См</a>
+            </div>
+            <div class="wrap_units" id="unit_two">
+                <h5 id="weight">Вес</h5>
+                <a href="#" id="kg">Кг</a>
+                <a href="#" id="tn">Тонна</a>
+            </div>
+        </div>
+    </div>
+
+</div>
